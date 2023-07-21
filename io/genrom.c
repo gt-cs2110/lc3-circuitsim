da86db94775ac7c7e549300e7b0c7f09aca2e1e5
Austin Adams <git@austinjadams.com>
2023-07-20 22:30:18 -0400

    Remove commented code

--- io/genrom.c
+++ io/genrom.c
@@ -73,27 +73,4 @@ int main(void) {
     fclose(fp1);
     fclose(fp2);
     return 0;
-    //write_zeros(' ');
-
-    //for (unsigned int i = 0; i < sizeof SevenSegmentASCII / sizeof *SevenSegmentASCII; i++) {
-    //    int bits = SevenSegmentASCII[i];
-    //    int g = (bits >> 6) & 1;
-    //    int f = (bits >> 5) & 1;
-    //    int e = (bits >> 4) & 1;
-    //    int d = (bits >> 3) & 1;
-    //    int c = (bits >> 2) & 1;
-    //    int b = (bits >> 1) & 1;
-    //    int a = bits & 1;
-
-    //    int roi = (e << 6) | (c << 5) | (f << 4) | (b << 3) | (d << 2) | (g << 1) | a;
-    //    printf("%02x", roi);
-    //    if (i % 16 == 15) {
-    //        putchar('\n');
-    //    } else {
-    //        putchar(' ');
-    //    }
-    //}
-
-    //// Upper ASCI bit is unused
-    //write_zeros(128);
 }
